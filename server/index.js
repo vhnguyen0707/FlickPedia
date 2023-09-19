e3bc1b6f6a770a2933cb079401ae4d818ecd2292
vhnguyen0707 <nhvu@ualberta.ca>
2023-09-19 15:31:06 -0700

    Revert to using cors()

--- server/index.js
+++ server/index.js
@@ -1,30 +1,13 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import http from 'http';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import routes from './src/routes/index.js';
 
 const app = express();
 
-const allowCors = (fn) => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-    return await fn(req, res);
-}
-app.use(allowCors);
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false})); //use basic querystring to parse url-encoded form data
 app.use(cookieParser());
@@ -36,7 +19,6 @@ const MONGODB_URL = process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URL
     : process.env.MONGODB_URL
 
-// const server = http.createServer(app);
 mongoose.set('strictQuery', false);
 mongoose.connect(MONGODB_URL).then(() => {
     console.log('Mongodb connected');
