761be0dd190ed874e094a9311b7dde8e3c6262d8
vhnguyen0707 <nhvu@ualberta.ca>
2023-09-19 15:08:43 -0700

    Change to using custom cors middleware

--- server/index.js
+++ server/index.js
@@ -7,16 +7,23 @@ import 'dotenv/config';
 import routes from './src/routes/index.js';
 
 const app = express();
-const port = process.env.PORT || 8000;
-const MONGODB_URL = process.env.NODE_ENV === 'test'
-    ? process.env.TEST_MONGODB_URL
-    : process.env.MONGODB_URL
 
-app.use(cors({
-    origin: 'https://flickpedia.vercel.app',
-    methods: 'GET, HEAD, POST, DELETE',
-    credentials: true
-}));
+const allowCors = (fn) => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    return await fn(req, res);
+}
+app.use(allowCors);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false})); //use basic querystring to parse url-encoded form data
@@ -24,6 +31,10 @@ app.use(cookieParser());
 
 app.use('/api', routes);
 
+const port = process.env.PORT || 8000;
+const MONGODB_URL = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URL
+    : process.env.MONGODB_URL
 
 // const server = http.createServer(app);
 mongoose.set('strictQuery', false);
